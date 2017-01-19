01e0e3db7ce450d67c643bb0fe36e3ba90438c1a
Nikhil Rayaprolu <nikhil684@gmail.com>
2017-01-20 00:14:19 +0530

    check

--- src/bug.ts
+++
@@ -1,7 +0,0 @@
-import {AppModule} from "./app/app.module";
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-export class bug {
-  callbug(){
-  platformBrowserDynamic().bootstrapModule(AppModule);
-}
-}
